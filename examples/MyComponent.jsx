9bdd19ea940b253e9ac125958fa681814b5e9bae
greginstruqt <141638762+greginstruqt@users.noreply.github.com>
2023-09-14 14:18:25 +0200

    Update MyComponent.jsx

--- examples/MyComponent.jsx
+++ examples/MyComponent.jsx
@@ -4,8 +4,10 @@ const MyComponent = (props) => {
   const { children, className } = props
   return (
     <iframe 
-      width="1140"
-      height="640"
+      style={{
+        width: "1140",
+        height: "640"
+      }}
       sandbox="allow-forms allow-modals allow-popups allow-same-origin allow-scripts"
       src="https://play.instruqt.com/embed/gregorio-test/tracks/repo-playground-poc-bun?token=em_hXRPTI6f6oLxqYmU" style="border: 0;"
       allowfullscreen>
