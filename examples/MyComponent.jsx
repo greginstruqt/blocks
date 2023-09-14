5218174d974a5e85b41063a9d79c93dcbc2607c0
greginstruqt <141638762+greginstruqt@users.noreply.github.com>
2023-09-14 14:22:09 +0200

    Update MyComponent.jsx

--- examples/MyComponent.jsx
+++ examples/MyComponent.jsx
@@ -6,12 +6,12 @@ const MyComponent = (props) => {
     <iframe 
       style={{
         width: "1140",
-        height: "640"
+        height: "640",
+        order: 0
       }}
       sandbox="allow-forms allow-modals allow-popups allow-same-origin allow-scripts"
-      src="https://play.instruqt.com/embed/gregorio-test/tracks/repo-playground-poc-bun?token=em_hXRPTI6f6oLxqYmU" style="border: 0;"
-      >
-  </iframe>
+      src="https://play.instruqt.com/embed/gregorio-test/tracks/repo-playground-poc-bun?token=em_hXRPTI6f6oLxqYmU">
+    </iframe>
   )
 }
 
