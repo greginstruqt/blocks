6835b76bb722f2821dc496232c00f3e6c18b5303
greginstruqt <141638762+greginstruqt@users.noreply.github.com>
2023-09-14 13:41:27 +0200

    Update MyComponent.jsx

--- examples/MyComponent.jsx
+++ examples/MyComponent.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const MyComponent = (props) => {
   const { children, className } = props
   return (
-    <div style={{
-      padding: "3em",
-      width: "100%",
-      background: "papayawhip",
-      position: "relative",  
-      boxSizing: "border-box",
-    }}>
-    </div>
-    <div className="App">
+    <div>
       <h3>Iframes in React</h3>
       <iframe src="https://platform.twitter.com/widgets/tweet_button.html"></iframe>
     </div>
